55499c8c4c534abfc6600b1ee844b40fbc2491e0
andyarensman <86622025+andyarensman@users.noreply.github.com>
2023-04-06 11:30:16 -0700

    first groq query

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
-import project from "./sanity/schemas/project-schema";
+import schemas from "./sanity/schemas";
 
 const config = defineConfig({
   projectId: "n079dr6w",
@@ -9,7 +9,7 @@ const config = defineConfig({
   apiVersion: "2023-04-05",
   basePath: "/admin",
   plugins: [deskTool()],
-  schema: { types: [project]}
+  schema: { types: schemas }
 })
 
 export default config;
---
+++ sanity/sanity-utils.ts
@@ -0,0 +1,21 @@
+import { createClient, groq } from "next-sanity";
+
+export async function getProjects() {
+  const client = createClient({
+    projectId: "n079dr6w",
+    dataset: "production",
+    apiVersion: "2023-04-05",
+  });
+
+  return client.fetch(
+    groq`*[_type == "project"]{
+      _id,
+      _createdAt,
+      name,
+      "slug": slug.current,
+      "image": image.asset->url,
+      url,
+      content
+    }`
+  )
+}
