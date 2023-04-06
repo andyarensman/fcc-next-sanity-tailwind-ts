2fa667fe9b617857fd816aa71ea5a9d52e6f79a7
Andrew Arensman <andrew.arensman@gmail.com>
2023-04-05 18:06:48 -0700

    first sanity schema

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
+import project from "./sanity/schemas/project-schema";
 
 const config = defineConfig({
   projectId: "n079dr6w",
@@ -8,6 +9,7 @@ const config = defineConfig({
   apiVersion: "2023-04-05",
   basePath: "/admin",
   plugins: [deskTool()],
+  schema: { types: [project]}
 })
 
 export default config;
