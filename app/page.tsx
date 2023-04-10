34d3a0b2b04e3c427d29cb9b8c32577425657502
andyarensman <86622025+andyarensman@users.noreply.github.com>
2023-04-10 12:18:10 -0700

    styling project pages

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,13 @@ export default async function Home() {
 
   return (
     <div className="max-w-5xl mx-auto py-20">
-      <h1 className="text-7xl font-extrabold">Hello, I&apos;m <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">Andy</span></h1>
+      <h1 className="text-7xl font-extrabold">
+        Hello, I&apos;m{" "}
+        <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">
+          Andy
+        </span>
+        !
+      </h1>
       <p className="mt-3 text-xl text-gray-600">Hello, everyone! Check out my projects</p>
 
       <h2 className="mt-24 font-bold text-gray-700 text-3xl">My Projects</h2>
